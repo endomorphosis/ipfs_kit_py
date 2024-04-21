cca143242202530eb179e2d6727c62b6af6afba0
endomorphosis <starworks5@gmail.com>
2024-04-20 17:53:20 -0700

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='orbitdb_kit',
+	version='0.0.1',
+	packages=[
+		'ipfs_kit',
+        's3_kit',
+        'ipfs_kit.ipfs_kit_lib',
+	],
+	install_requires=[
+	
+    ]
+)
