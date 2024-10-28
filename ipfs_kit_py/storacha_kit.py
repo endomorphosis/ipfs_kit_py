b5542adc580702d78babc6ddfece70601048babc
endomorphosis <starworks5@gmail.com>
2024-10-27 17:37:13 -0700

    Add storacha_kit class with test method

---
+++ ipfs_kit_py/storacha_kit.py
@@ -0,0 +1,12 @@
+
+class storacha_kit:
+    def __init__(self, resources=None, meta=None):
+        self.resources = resources
+        self.meta = meta
+        
+    def test():
+        print("storacha_kit test")
+        return True
+
+if __name__ == "__main__":
+    storacha_kit.test()
